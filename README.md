0aa09e9b324825d5e7d7d144bf97ed027fd33265
Ryan Delaney <ryan.patrick.delaney@protonmail.com>
2021-01-20 16:16:02 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,8 @@
+# Bigger Mining Drill Status Lights Continued
+
+Makes bigger mining drill status lights, duh.
+
+Like 4x the size.
+
+A continuation of the [original mod](https://mods.factorio.com/mod/BiggerMiningDrillStatusLights)
+by Kamel.
---
+++ bigger-mining-drill-status-lights-continued/data.lua
@@ -0,0 +1,17 @@
+data.raw["mining-drill"]["electric-mining-drill"]["graphics_set"]["working_visualisations"][8]["north_animation"]["scale"] = 4;
+data.raw["mining-drill"]["electric-mining-drill"]["graphics_set"]["working_visualisations"][8]["east_animation"]["scale"] = 4;
+data.raw["mining-drill"]["electric-mining-drill"]["graphics_set"]["working_visualisations"][8]["south_animation"]["scale"] = 4;
+data.raw["mining-drill"]["electric-mining-drill"]["graphics_set"]["working_visualisations"][8]["west_animation"]["scale"] = 4;
+data.raw["mining-drill"]["electric-mining-drill"]["graphics_set"]["working_visualisations"][8]["north_animation"]["hr_version"]["scale"] = 2;
+data.raw["mining-drill"]["electric-mining-drill"]["graphics_set"]["working_visualisations"][8]["east_animation"]["hr_version"]["scale"] = 2;
+data.raw["mining-drill"]["electric-mining-drill"]["graphics_set"]["working_visualisations"][8]["south_animation"]["hr_version"]["scale"] = 2;
+data.raw["mining-drill"]["electric-mining-drill"]["graphics_set"]["working_visualisations"][8]["west_animation"]["hr_version"]["scale"] = 2;
+
+data.raw["mining-drill"]["electric-mining-drill"]["wet_mining_graphics_set"]["working_visualisations"][14]["north_animation"]["scale"] = 4;
+data.raw["mining-drill"]["electric-mining-drill"]["wet_mining_graphics_set"]["working_visualisations"][14]["east_animation"]["scale"] = 4;
+data.raw["mining-drill"]["electric-mining-drill"]["wet_mining_graphics_set"]["working_visualisations"][14]["south_animation"]["scale"] = 4;
+data.raw["mining-drill"]["electric-mining-drill"]["wet_mining_graphics_set"]["working_visualisations"][14]["west_animation"]["scale"] = 4;
+data.raw["mining-drill"]["electric-mining-drill"]["wet_mining_graphics_set"]["working_visualisations"][14]["north_animation"]["hr_version"]["scale"] = 2;
+data.raw["mining-drill"]["electric-mining-drill"]["wet_mining_graphics_set"]["working_visualisations"][14]["east_animation"]["hr_version"]["scale"] = 2;
+data.raw["mining-drill"]["electric-mining-drill"]["wet_mining_graphics_set"]["working_visualisations"][14]["south_animation"]["hr_version"]["scale"] = 2;
+data.raw["mining-drill"]["electric-mining-drill"]["wet_mining_graphics_set"]["working_visualisations"][14]["west_animation"]["hr_version"]["scale"] = 2;
